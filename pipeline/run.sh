7622657824a636b7d7a60530e2dcf755fba3f2d8
Ma Viet Duc <95759699+maduc238@users.noreply.github.com>
2023-01-29 10:50:23 +0700

    Update run.sh

--- pipeline/run.sh
+++ pipeline/run.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
-python3 pipeline.py --rank=0 --world_size=2 --interface=lo --master_addr=localhost --master_port=29500 &
-python3 pipeline.py --rank=1 --world_size=2 --interface=lo --master_addr=localhost --master_port=29500
+
+# Variables:
+file_name='pipeline.py'
+user='master'
+rank=0
+master_addr='localhost'
+interface='lo'
+split=8
+
+taskset -c 0 python3 $file_name --rank=0 --world_size=2 --interface=$interface --master_addr=$master_addr --master_port=29500 --split=$split &
+taskset -c 1 python3 $file_name --rank=1 --world_size=2 --interface=$interface --master_addr=$master_addr --master_port=29500 --split=$split
